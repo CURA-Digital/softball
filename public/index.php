29d0c745f13fc07a508bc7f3ca4f0e9d04afd170
Speshled <chasebailly@gmail.com>
2024-01-11 22:23:06 -0700

    positions in game defined by player count

--- public/index.php
+++ public/index.php
@@ -6,13 +6,38 @@
     <body>
         <?php
         // Define softball positions and players
-        $positions = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'LF', 'CLF', 'CRF', 'RF'];
         $players = ['Bentley', 'Alyssa', 'Chloe', 'Elaina', 'Emmy', 'Harper', 'Isla', 'Katelyn', 'Lily', 'Vanessa'];
+        $positions6 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd'];
+        $positions7 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'CF'];
+        $positions8 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'LF', 'RF'];
+        $positions9 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'LF', 'CF', 'RF'];
+        $positions10 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'LF', 'CLF', 'CRF', 'RF'];
+        $positions11 = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd', 'LF', 'CLF', 'CF', 'CRF', 'RF'];
+        $infield = ['Pitcher', 'Catcher', '1st', '2nd', 'SS', '3rd'];
+        $outfield = ['LF', 'CLF', 'CF', 'CRF', 'RF'];
 
-        // Make sure there are enough players for all positions
-        if (count($players) < count($positions)) {
-            echo "Not enough players for all positions.";
-            exit;
+        // Choose positions based on number of players
+        switch (count($players)) {
+            case 6:
+                $positions = $positions6;
+                break;
+            case 7:
+                $positions = $positions7;
+                break;
+            case 8:
+                $positions = $positions8;
+                break;
+            case 9:
+                $positions = $positions9;
+                break;
+            case 10:
+                $positions = $positions10;
+                break;
+            case 11:
+                $positions = $positions11;
+                break;
+            default:
+                $positions = $positions6;
         }
 
         // Initialize an empty team array
